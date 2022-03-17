ab3c7a839a86be34a3d7bfc962c04168e07c27d9
liuyanyi <wolfsonliu@163.com>
2022-03-17 16:59:22 +0800

    add csl part for angel coder

--- demo_angel_code.py
+++ demo_angel_code.py
@@ -1,30 +1,36 @@
 import math
-import numpy as np
+
 import matplotlib.pyplot as plt
+import numpy as np
 import torch
-from mmrotate.models.dense_heads.rotated_csl_retina_head import RotatedCSLRetinaHead
+
+from mmrotate.models.utils.angel_coder import AngelCoder
 
 
 def main():
-    head = RotatedCSLRetinaHead(num_classes=15,
-                                in_channels=256,
-                                label_type='csl',
-                                label_mode='triangle',
-                                omega=1,
-                                radius=6,
-                                angle_version='le90')
+    # head = RotatedCSLRetinaHead(num_classes=15,
+    #                             in_channels=256,
+    #                             label_type='csl',
+    #                             label_mode='gaussian',
+    #                             omega=1,
+    #                             radius=6,
+    #                             angle_version='le90')
 
-    angel_target = torch.tensor([[math.pi / 2]])
+    coder = AngelCoder(
+        type='csl', window='gaussian', omega=1, radius=6, angle_version='le90')
 
-    encode = head.circular_encode(angel_target)
+    angel_target = torch.tensor([[math.pi / 4]])
 
-    decode = head.circular_decode(encode)
+    encode = coder.encode(angel_target)
+
+    decode = coder.decode(encode)
 
     encode_np = encode[0].numpy()
     x = np.arange(-90, 90)
     plt.plot(x, encode_np)
     plt.show()
-    print('decode-target=', decode-angel_target)
+    print('decode-target=', decode - angel_target)
+
 
 if __name__ == '__main__':
     main()
--- mmrotate/models/utils/angel_coder.py
+++ mmrotate/models/utils/angel_coder.py
@@ -1,4 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import math
+
+import torch
+
+
 class AngelCoder:
     """Angel Coder for Angel Coded Method such as CSL and DCL.
 
@@ -7,7 +12,7 @@ class AngelCoder:
 
     Args:
         type:
-        angel_type:
+        angle_version:
         category_deg:
         window:
         radius:
@@ -15,24 +20,22 @@ class AngelCoder:
 
     def __init__(self,
                  type,
-                 angel_type,
-                 category_deg=1,
+                 angle_version,
+                 omega=1,
                  window='gaussian',
                  radius=6):
         self.type = type
-        self.angel_type = angel_type
-        if angel_type == 'oc':
-            self.angel_range = 90
-            self.angel_offset = 90
-        elif angel_type == 'le90':
-            self.angel_range = 180
-            self.angel_offset = 90
-        elif angel_type == 'le135':
-            self.angel_range = 180
-            self.angel_offset = 45
-        self.category_deg = category_deg
+        self.angle_version = angle_version
+        assert angle_version in ['oc', 'le90', 'le135']
+        self.angle_range = 90 if angle_version == 'oc' else 180
+        self.angle_offset = 45 if angle_version == 'le135' else 90
+        self.omega = omega
         self.window = window
         self.radius = radius
+        if self.type == 'csl':
+            self.coding_len = int(self.angle_range // omega)
+        else:
+            raise NotImplementedError
 
     # @property
     # def coding_length(self):
@@ -41,7 +44,7 @@ class AngelCoder:
         if self.type == 'csl':
             return self.csl_encode(angel_targets)
         elif self.type == 'dcl':
-            pass
+            raise NotImplementedError
         else:
             raise NotImplementedError
 
@@ -49,12 +52,61 @@ class AngelCoder:
         if self.type == 'csl':
             return self.csl_decode(angel_preds)
         elif self.type == 'dcl':
-            pass
+            raise NotImplementedError
         else:
             raise NotImplementedError
 
-    def csl_encode(self, angel_targets):
-        pass
+    def csl_encode(self, angle_targets):
+        # Radius To Degree
+        angle_targets_deg = angle_targets * (180 / math.pi)
+        # Empty Label
+        smooth_label = torch.zeros_like(angle_targets).repeat(
+            1, self.coding_len)
+        angle_targets_deg = (angle_targets_deg +
+                             self.angle_offset) / self.omega
+        # TODO 要不要四舍五入，这里是直接舍掉，解码再+0.5
+        angle_targets_long = angle_targets_deg.long()
+
+        if self.window == 'pulse':
+            radius_range = angle_targets_long
+            smooth_value = 1.0
+        elif self.window == 'rect':
+            base_radius_range = torch.arange(
+                -self.radius, self.radius, device=angle_targets_long.device)
+            radius_range = (base_radius_range +
+                            angle_targets_long) % self.coding_len
+            smooth_value = 1.0
+        elif self.window == 'triangle':
+            base_radius_range = torch.arange(
+                -self.radius, self.radius, device=angle_targets_long.device)
+            radius_range = (base_radius_range +
+                            angle_targets_long) % self.coding_len
+            smooth_value = 1.0 - torch.abs(
+                (1 / self.radius) * base_radius_range)
+
+        elif self.window == 'gaussian':
+            base_radius_range = torch.arange(
+                -self.angle_range // 2,
+                self.angle_range // 2,
+                device=angle_targets_long.device)
+
+            radius_range = (base_radius_range +
+                            angle_targets_long) % self.coding_len
+            smooth_value = torch.exp(-torch.pow(base_radius_range, 2) /
+                                     (2 * self.radius**2))
+
+        else:
+            raise NotImplementedError
+
+        # 调整维度
+        if isinstance(smooth_value, torch.Tensor):
+            smooth_value = smooth_value.unsqueeze(0).repeat(
+                smooth_label.size(0), 1)
+
+        return smooth_label.scatter(1, radius_range, smooth_value)
 
     def csl_decode(self, angel_preds):
-        pass
+        angle_cls_inds = torch.argmax(angel_preds, dim=1)
+        angle_pred = ((angle_cls_inds + 0.5) *
+                      self.omega) % self.angle_range - self.angle_offset
+        return angle_pred * (math.pi / 180)
